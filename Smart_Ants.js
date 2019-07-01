26ab6e221056c745df3d97a8d0a2106a8d6559e7
Kaleb Campbell <kaleb1997@xtra.co.nz>
2019-07-02 01:08:27 +1200

    Add files via upload

--- Smart_Ants.js
+++ Smart_Ants.js
@@ -1,6 +1,6 @@
 var population;
 var lifespan = 200;
-//var lifeP;
+var lifeP;
 var count = 0;
 var target;
 
@@ -8,7 +8,6 @@ function setup() {
   createCanvas(400, 300);
   ant = new Ant();
   population = new Population();
-  //lifeP.html(count);
   target = createVector(width/2, 15);
 }
 
@@ -16,11 +15,13 @@ function setup() {
 function draw() {
   background(0);
   population.run();
-  //lifeP.html(count);
+  //lifeP.HTML(count);
 
   count++;
   if (count == lifespan) {
-    population = new Population();
+    population.evaluate();
+    population.selection();
+    //population = new Population();
     count = 0;
   }
 
@@ -30,50 +31,102 @@ function draw() {
 function Population() {
   this.ants = [];
   this.popsize = 25;
+  this.matingpool = [];
 
   for (var i = 0; i < this.popsize; i++) {
     this.ants[i] = new Ant();
   }
 
-  this.evaluate = function(){
+  this.evaluate = function() {
+
+    var maxfit = 0;
     for (var i = 0; i < this.popsize; i++) {
       this.ants[i].calcFitness();
+      if (this.ants[i].fitness > maxfit) {
+        maxfit = this.ants[i].fitness;
+      }
     }
-  }
+
+    for (var k = 0; k < this.popsize; k++) {
+      this.ants[k].fitness /= maxfit;
+    }
+    this.matingpool = [];
+
+    for (var l = 0; l < this.popsize; l++) {
+      var n = this.ants[l].fitness * 100;
+      for (var j = 0; j < n; j++) {
+        this.matingpool.push(this.ants[l]);
+      }
+    }
+  };
+
+  this.selection = function() {
+
+    var newAnts = [];
+    for (var i = 0; i < this.ants.length; i++) {
+      //gets random element from array
+      var parentA = random(this.matingpool).dna;
+      var parentB = random(this.matingpool).dna;
+
+      var child = parentA.crossover(parentB);
+
+      newAnts[i] = new Ant(child);
+    }
+    this.ants = newAnts;
+  };
 
   this.run = function() {
     for (var i = 0; i < this.popsize; i++) {
       this.ants[i].update();
       this.ants[i].show();
     }
-  }
+  };
 }
 
-function DNA() {
-  this.genes = [];
-  for (var i =0; i < lifespan; i++) {
-    this.genes[i] = p5.Vector.random2D();
-    this.genes[i].setMag(0.1);
+function DNA(genes) {
+  if (genes) {
+    this.genes = genes;
+  } else {
+    this.genes = [];
+    for (var i =0; i < lifespan; i++) {
+      this.genes[i] = p5.Vector.random2D();
+      this.genes[i].setMag(0.1);
+    }
   }
+  this.crossover = function(partner) {
+    var newgenes = [];
+    var mid = floor(random(this.genes.length));
+    for (var i = 0; i < this.genes.length; i++) {
+      if (i > mid) {
+        newgenes[i] = this.genes[i];
+      } else {
+        newgenes[i] = partner.genes[i];
+      }
+    }
+    return new DNA(newgenes);
+  };
 }
 
-function Ant() {
+function Ant(dna) {
   this.pos = createVector(width/2, height);
   this.vel = createVector();
   this.acc = createVector();
-  this.dna = new DNA();
+  if (dna) {
+    this.dna = dna;
+  } else {
+    this.dna = new DNA();
+  }
   this.fitness = 0;
 
 
   this.applyForce = function(force) {
     this.acc.add(force);
-  }
-  
-  this.calcFitness = function(){
+  };
+
+  this.calcFitness = function() {
     var d = dist(this.pos.x, this.pos.y, target.x, target.y);
-    this.fitness = 1/d;
-    
-  }
+    this.fitness = map(d, 0, width, width, 0);
+  };
 
   this.update = function() {
     this.applyForce(this.dna.genes[count]);
@@ -81,7 +134,7 @@ function Ant() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0);
-  }
+  };
 
   this.show = function() {
     push();
@@ -92,5 +145,5 @@ function Ant() {
     rectMode(CENTER);
     rect(0, 0, 25, 5);
     pop();
-  }
+  };
 }
